1c3977b42e3da12c8108df0b0628abcec077de9c
ThalysRD <tgbr66@gmail.com>
2024-09-22 12:08:22 -0300

    Feat: Remove todos os valores ímpares da lista

---
+++ src/Desafio4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Desafio4 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> positivos = numeros.stream().filter(n -> n % 2 ==0).sorted().toList();
+        positivos.forEach(System.out::println);
+    }
+}
